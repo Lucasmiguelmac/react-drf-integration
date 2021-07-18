62eea5009a2e0e3d589aae93352dafe0f65bf47e
lucas <lucasmiguelmac@gmail.com>
2021-07-18 23:06:57 +0000

    Create Movie serializer

---
+++ backend/movies/serializers.py
@@ -0,0 +1,11 @@
+from django.db.models import fields
+from rest_framework import serializers
+
+from movies.models import Movie
+
+
+class MovieSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Movie
+        fields = '__all__'
