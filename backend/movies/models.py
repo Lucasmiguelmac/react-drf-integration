03f8c6206a3ea765780fabb756c0afa59e844d10
lucas <lucasmiguelmac@gmail.com>
2021-07-18 21:13:02 +0000

    Set movie genre choices

--- backend/movies/models.py
+++ backend/movies/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
 
+class GenreChoices(models.TextChoices):
+    ACTION  = "Action"
+    COMEDY  = "Comedy"
+    DRAMA   = "Drama"
+
 class Movie(models.Model):
     title           = models.CharField(max_length=300)
     release_date    = models.DateField()
-    genre           = models.CharField(max_length=123)
+    genre           = models.CharField(choices=GenreChoices.choices, max_length=123)
     plot            = models.TextField()
