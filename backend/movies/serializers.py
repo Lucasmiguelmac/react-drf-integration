ed203bdff50e053bc8cf4031f5d8ed04f1773ee2
lucas <lucasmiguelmac@gmail.com>
2021-07-18 23:39:58 +0000

    Create rating serializer

--- backend/movies/serializers.py
+++ backend/movies/serializers.py
@@ -1,11 +1,20 @@
 from django.db.models import fields
 from rest_framework import serializers
 
-from movies.models import Movie
+from movies.models import Movie, Rating
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    
 
     class Meta:
         model = Movie
         fields = '__all__'
+
+
+class RatingSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
+
+    class Meta:
+        model = Rating
+        exclude = ('movie',)
