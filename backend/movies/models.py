4d7f13ac8db074a69a60f62b57db53ab0436a1fd
lucas <lucasmiguelmac@gmail.com>
2021-07-18 21:39:14 +0000

    Change GenreChoices name to MovieGenre

--- backend/movies/models.py
+++ backend/movies/models.py
@@ -1,7 +1,9 @@
+from django.core import validators
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
-class GenreChoices(models.TextChoices):
+class MovieGenre(models.TextChoices):
     ACTION  = "Action"
     COMEDY  = "Comedy"
     DRAMA   = "Drama"
@@ -9,5 +11,5 @@ class GenreChoices(models.TextChoices):
 class Movie(models.Model):
     title           = models.CharField(max_length=300)
     release_date    = models.DateField()
-    genre           = models.CharField(choices=GenreChoices.choices, max_length=123)
+    genre           = models.CharField(choices=MovieGenre.choices, max_length=123)
     plot            = models.TextField()
