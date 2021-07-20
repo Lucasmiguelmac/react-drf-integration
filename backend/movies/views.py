9b7826ca6d0bf9d7cc0bbe15c7abbb07c2b3087a
lucas <lucasmiguelmac@gmail.com>
2021-07-20 17:22:52 +0000

    Create Movie viewset

--- backend/movies/views.py
+++ backend/movies/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+
+from movies import models as mm
+from . import serializers as ms
 
 # Create your views here.
+class MovieViewset(ModelViewSet):
+    queryset = mm.Movie.objects.all().order_by('-release_date')
+    serializer_class = ms.MovieSerializer
