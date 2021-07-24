6881aa86b948262f92ceb1e2a01e304bfa2148df
lucas <lucasmiguelmac@gmail.com>
2021-07-24 13:59:51 +0000

    Embedded movies list component into app using hardcoded data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,48 @@
 import React, { useState } from 'react';
-
+import MovieList from './components/movieList';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [movies, setMovies] = useState([
+    {
+      "ratings":[
+        {
+            "id":1,
+            "user":"c95326dd626b4e5a9528ceb804d8a9",
+            "rating":4,
+            "comment":"best movie ever"
+        }
+      ],
+      "title":"Spiderman",
+      "release_date":"2021-07-24",
+      "genre":"Action",
+      "plot":"asdsadsad",
+      "image":"https://cronicaglobal.elespanol.com/uploads/s1/61/11/51/3/main-700b9bff30_11_1000x528.jpeg"
+    },
+    {
+      "ratings": [
+        {}
+      ],
+      "title": "Mí título",
+      "release_date": "2020-02-02",
+      "genre": "Action",
+      "plot": "fdsfdsfds",
+      "image": "https://st4.depositphotos.com/14953852/22772/v/600/depositphotos_227725020-stock-illustration-no-image-available-icon-flat.jpg"
+    },
     {
       "ratings": [],
       "title": "Mí título",
       "release_date": "2020-02-02",
       "genre": "Action",
-      "plot": "fdsfdsfds"
+      "plot": "fdsfdsfds",
+      "image": "https://st4.depositphotos.com/14953852/22772/v/600/depositphotos_227725020-stock-illustration-no-image-available-icon-flat.jpg"
     }
   ]);
   return (
     <div>
-      Hola
+      <MovieList movies={movies} />
     </div>
   )
 }
 
-export default App
+export default App;
