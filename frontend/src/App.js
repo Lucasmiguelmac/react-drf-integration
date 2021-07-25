b75518f8413806dfea1c28fe16b0b9e815fcf1af
lucas <lucasmiguelmac@gmail.com>
2021-07-25 23:11:23 +0000

    Routify homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter } from "react-router-dom";
-import MovieList from './components/movieList';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from "axios";
+import { BrowserRouter, Route } from "react-router-dom";
 
 import "./App.css";
+import Home from "./pages/Home";
 import NavBar from './components/navBar';
 
+
 function App() {
-  const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    axios.get(`http://localhost:8000/api/movies`)
-    .then(resp => setMovies(resp.data))
-  }, [])
   return (
-    <BrowserRouter>
-      <NavBar />
-      <MovieList movies={movies} />
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <NavBar />
+        <Route path="/" component={Home} exact/>
+      </BrowserRouter>
+    </>
   )
 }
 
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from "axios";
+
+import MovieList from "../components/movieList";
+
+
+const Home = () => {
+    const [movies, setMovies] = useState([]);
+    useEffect(() => {
+        axios.get(`http://localhost:8000/api/movies`)
+        .then(resp => setMovies(resp.data))
+        window.scroll(0,0)
+    }, [])
+    return (
+        <div>
+            <h1>Home</h1>
+            <MovieList movies={movies} />
+        </div>
+    )
+}
+
+export default Home
