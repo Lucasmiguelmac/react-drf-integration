10757b33d128b7ef214f27b1e5df4009678092a6
lucas <lucasmiguelmac@gmail.com>
2021-07-25 23:32:33 +0000

    Set 404 not found page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import "./App.css";
 import Home from "./pages/Home";
 import MovieDetail from "./pages/MovieDetail";
 import NavBar from './components/navBar';
+import NotFound from "./pages/NotFound";
 
 
 function App() {
@@ -11,8 +12,11 @@ function App() {
     <>
       <BrowserRouter>
         <NavBar />
-        <Route path="/" component={Home} exact/>
-        <Route path="/detail" component={MovieDetail}/>
+        <Switch>
+          <Route path="/" component={Home} exact/>
+          <Route path="/detail" component={MovieDetail}/>
+          <Route component={NotFound} />
+        </Switch>
       </BrowserRouter>
     </>
   )
---
+++ frontend/src/pages/NotFound.js
@@ -0,0 +1,11 @@
+const NotFound = () => {
+    return (
+        <>
+            <h1>Not Found</h1>
+            <p>The page you are looking for does not exist.</p>
+            <img src={'https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif'} alt="loading..."></img>
+        </>
+    )
+}
+
+export default NotFound
