e7d2fe8b8d2fb78bf673ff2da98ac0286c9bafc3
lucas <lucasmiguelmac@gmail.com>
2021-07-25 15:33:48 -0300

    Switch from hardcded data to API data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,51 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import MovieList from './components/movieList';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from "axios";
 
 import "./App.css";
+import NavBar from './components/navBar';
 
 function App() {
-  const [movies, setMovies] = useState([
-    {
-      "ratings":[
-        {
-            "id":1,
-            "user":"c95326dd626b4e5a9528ceb804d8a9",
-            "rating":4,
-            "comment":"best movie ever"
-        }
-      ],
-      "title":"Spiderman",
-      "release_date":"2021-07-24",
-      "genre":"Action",
-      "plot":"asdsadsad",
-      "image":"https://cronicaglobal.elespanol.com/uploads/s1/61/11/51/3/main-700b9bff30_11_1000x528.jpeg"
-    },
-    {
-      "ratings": [
-        {}
-      ],
-      "title": "Mí título",
-      "release_date": "2020-02-02",
-      "genre": "Action",
-      "plot": "fdsfdsfds",
-      "image": "https://st4.depositphotos.com/14953852/22772/v/600/depositphotos_227725020-stock-illustration-no-image-available-icon-flat.jpg"
-    },
-    {
-      "ratings": [],
-      "title": "Mí título",
-      "release_date": "2020-02-02",
-      "genre": "Action",
-      "plot": "fdsfdsfds",
-      "image": "https://st4.depositphotos.com/14953852/22772/v/600/depositphotos_227725020-stock-illustration-no-image-available-icon-flat.jpg"
-    }
-  ]);
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    axios.get(`http://localhost:8000/api/movies`)
+    .then(resp => setMovies(resp.data))
+  }, [])
   return (
-    <div className="container-fluid overflow">
-      <div className="row">
-        <MovieList movies={movies} />
+    <>
+      <NavBar />
+      <div className="container-fluid overflow">
+        <div className="row">
+          <MovieList movies={movies} />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
--- frontend/src/components/movieList.jsx
+++ frontend/src/components/movieList.jsx
@@ -4,7 +4,6 @@ const MovieList = ({movies}) => {
     return (
         <>
             {movies.map((movie, index)=>{
-                console.log(movie)
                 return (
                     <div className="d-flex justify-content-start m-3" key={index} style={{height: "24rem", width:"16rem"}}>
                         <img src={movie.image} alt="" style={{height: "24rem", width:"16rem", objectFit: "cover"}}/>
