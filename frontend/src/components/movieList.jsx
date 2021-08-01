16b2d2a697036bb476005715e1061e4f04040eab
lucas <lucasmiguelmac@gmail.com>
2021-08-01 02:21:51 +0000

    Add title to movies in carousel

--- frontend/src/components/movieList.jsx
+++ frontend/src/components/movieList.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const MovieList = ({movies, movieListTitle}) => {
+    const movieTitleStyle = {
+        textShadow: "2px 0 0 #fff, -2px 0 0 #fff, 0 2px 0 #fff, 0 -2px 0 #fff, 1px 1px #fff, -1px -1px 0 #fff, 1px -1px 0 #fff, -1px 1px 0 #fff"
+    }
     return (
         <>
             <h1>{movieListTitle}</h1>
@@ -9,8 +12,11 @@ const MovieList = ({movies, movieListTitle}) => {
                 <div className="row">
                     {movies.map((movie, index)=>{
                         return (
-                            <div className="d-flex justify-content-start m-3" key={index} style={{height: "24rem", width:"16rem"}}>
+                            <div className="d-flex justify-content-start m-3" key={index} style={{height: "24rem", width:"16rem", position: "relative"}}>
                                 <Link to={`/${movie.slug}/`}>
+                                    <div className="title" style={{position: "absolute", bottom: "1rem", left: "2rem"}}>
+                                        <h2 style={movieTitleStyle}>{movie.title.toUpperCase()}</h2>
+                                    </div>
                                     <img src={movie.image} alt="" style={{height: "24rem", width:"16rem", objectFit: "cover"}}/>
                                 </Link>
                             </div>
