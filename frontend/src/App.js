e03e76f4e416cba7e239d9119ba3b1d0cb28677c
lucas <lucasmiguelmac@gmail.com>
2021-08-09 09:28:46 -0300

    Setup login form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,28 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { CookiesProvider } from "react-cookie";
 
 import "./App.css";
 import Home from "./pages/Home";
 import MovieDetail from "./pages/MovieDetail";
 import NavBar from './components/navBar';
 import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
 
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <NavBar />
-        <Switch>
-          <Route path="/" component={Home} exact/>
-          <Route path="/:slug" component={MovieDetail}/>
-          <Route component={NotFound} />
-        </Switch>
-      </BrowserRouter>
+      <CookiesProvider>
+        <BrowserRouter>
+          <NavBar />
+          <Switch>
+            <Route path="/login" component={Login}/>
+            <Route path="/" component={Home} exact/>
+            <Route path="/:slug" component={MovieDetail}/>
+            <Route component={NotFound} />
+          </Switch>
+        </BrowserRouter>
+      </CookiesProvider>
     </>
   )
 }
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,79 @@
+import axios from "axios";
+import { useState } from "react";
+import { useCookies } from "react-cookie";
+import { useHistory } from "react-router-dom";
+
+const Login = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [token, setToken] = useCookies(['loginToken'])
+    
+    const headers = {
+        'Content-Type': 'application/json',
+    }
+    const handleLogin = () => {
+        const data = {
+            email: email,
+            password: password
+        };
+        axios.post(`http://localhost:8000/api/login/`, data, {"headers": headers})
+        .then(response => setToken('loginToken', response))
+        .catch(error => console.log(error))
+    };
+
+    return (
+        <div style={{width: "100vw", height: "100vh",margin: 0, display: "flex", alignItems: "center", justifyContent: "center", backgroundColor: "#F5F5F5"}}>
+            <div className="card" style={{}}>
+                <div className="card-body">
+                    <form>
+                        <div className="mb-3">
+                            <label htmlFor="exampleInputEmail1" className="form-label">
+                            Email address
+                            </label>
+                            <input
+                            type="email"
+                            className="form-control"
+                            id="exampleInputEmail1"
+                            aria-describedby="emailHelp"
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                            />
+                            <div id="emailHelp" className="form-text">
+                            We'll never share your email with anyone else.
+                            </div>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="exampleInputPassword1" className="form-label">
+                            Password
+                            </label>
+                            <input
+                            type="password"
+                            className="form-control"
+                            id="exampleInputPassword1"
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                            />
+                        </div>
+                        <button
+                        type="button"
+                        className="btn btn-primary"
+                        onClick={handleLogin}
+                        >
+                            Log In
+                        </button>
+                        <button
+                        type="button"
+                        className="btn btn-link"
+                        style={{marginLeft: "1rem"}}
+                        >
+                            Register
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
